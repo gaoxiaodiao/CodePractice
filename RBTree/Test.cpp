b324876d67446c391510349bd2b1fdc3124a89fe
gaoxiaodiao <805888196@qq.com>
2017-04-21 23:13:56 +0800

    测试文件

--- RBTree/Test.cpp
+++ RBTree/Test.cpp
@@ -1,44 +1,26 @@
+/*
+*文件说明:测试红黑树
+*作者:高小调
+*日期:2017-04-21
+*集成开发环境:Microsoft Visual Studio 2010 
+*/
 #include<iostream>
 using namespace std;
-#include"RBTree6.h"
-void Test(){
-	RBTree<int,int> t;
-	int arr[2][10] = {{2,4,5,3,6,9,1,0,7,8},
-					{4,2,6,1,3,5,15,7,16,14}
-	};
-	for(int j=0; j<2; ++j){
-		for(int i=0; i<sizeof(arr[0])/sizeof(arr[0][0]); ++i){
-			t.insert(arr[j][i],i);
-			std::cout<<arr[j][i]<<":"<<t.isBalance()<<" ";
+#include"RBTree.hpp"
+void TestRBTree(){
+	RBTree<int,int> rbt;
+	int arr[][10] = {{2,4,5,3,6,9,1,0,7,8},
+					{4,2,6,1,3,5,15,7,16,14}};
+	for(int i=0; i<sizeof(arr)/sizeof(*arr); ++i){
+		for(int j=0; j<sizeof(*arr)/sizeof(**arr); ++j){
+			rbt.Insert(arr[i][j],i);
+			std::cout<<arr[i][j]<<":"<<rbt.CheckIsBalance()<<std::endl;
 		}
-		cout<<endl;
+		rbt.InOrderNonR();
+		std::cout<<std::endl;
 	}
-	//t.InOrder();
 }
 int main(){
-	Test();
+	TestRBTree();
 	return 0;
 }
-#if 0
-#include<iostream>
-using namespace std;
-#include"RBTree2.h"
-void Test(){
-	RBTree<int,int> t;
-	int arr[2][10] = {{2,4,5,3,6,9,1,0,7,8},
-					{4,2,6,1,3,5,15,7,16,14}
-	};
-	for(int j=0; j<2; ++j){
-		for(int i=0; i<sizeof(arr[0])/sizeof(arr[0][0]); ++i){
-			t.Insert(arr[j][i],i);
-			std::cout<<arr[j][i]<<":"<<t.IsBalance()<<" ";
-		}
-		cout<<endl;
-	}
-	//t.InOrder();
-}
-int main(){
-	Test();
-	return 0;
-}
-#endif

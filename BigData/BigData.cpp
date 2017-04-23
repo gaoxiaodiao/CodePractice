c896145a93654ab6480bb3ca1bf106a723b7d971
gaoxiaodiao <805888196@qq.com>
2017-04-23 22:10:35 +0800

    大数据处理相关实现

--- BigData/BigData.cpp
+++ BigData/BigData.cpp
@@ -12,15 +12,19 @@ BigData::BigData()
 //整型参数的构造函数
 BigData::BigData(INT64 value)
 	:_intValue(value){
-	_strValue.resize(20);
-	_Int64ToString(value);
+	int count = 2;
+	_strValue.resize(MaxSize);
+	_Int64ToString(value,count);
+	_strValue.resize(count);
 }
 //字符串构造
 BigData::BigData(const char *strValue)
 	:_intValue(0){
 	size_t len = std::strlen(strValue);
-	_strValue.resize(len+1);	//还有符号位
-	_StringToData(strValue);
+	int count = 2;
+	_strValue.resize(len+2);	//还有符号位
+	_StringToData(strValue,count);
+	_strValue.resize(count);
 }
 //去除空格与0
 char* BigData::_DeleteBlank(const char* str){
@@ -31,7 +35,7 @@ char* BigData::_DeleteBlank(const char* str){
 }
 
 //将整型数字转化为字符串,并存入_strValue
-void BigData::_Int64ToString(INT64 value){
+void BigData::_Int64ToString(INT64 value,int &count){
 	//处理符号
 	_strValue[0] = value>=0?'+':'-';
 	//处理数字
@@ -40,11 +44,13 @@ void BigData::_Int64ToString(INT64 value){
 	if(value==0){
 		//value为0,进行特殊处理
 		*str = '0';
+		count++;
 		return ;
 	}else if(value == MinValue){
 		//value为负数最大值,无法转为正数
 		//在此进行特殊处理
 		strcpy(str,"9223372036854775808");
+		count = MaxSize;
 		return ;
 	}else if(value <0){
 		//如果value为负数,则将其转换为正数进行处理
@@ -55,6 +61,7 @@ void BigData::_Int64ToString(INT64 value){
 	while(value){
 		*str++ = value%10 +'0';
 		value /= 10;
+		count++;
 	}
 	//字符串逆序,把逆序的数字颠倒过来,让它变成正序的.
 	char *end = str-1;
@@ -66,16 +73,16 @@ void BigData::_Int64ToString(INT64 value){
 		end --;
 	}
 }
-void BigData::_StringToData(const char *str){
+void BigData::_StringToData(const char *str,int& count){
 	char symbol = '+';
 	char *cur = (char*)str;
-	int count = 0;
 	//去除空格与多余的0("     000123456")
 	cur = _DeleteBlank(cur);
 	//（"00000000000"）
 	if(*cur=='\0'){
 		_strValue[0] = '+';
 		_strValue[1] = '0';
+		count++;
 		return ;
 	}
 	//如果有符号
@@ -89,6 +96,7 @@ void BigData::_StringToData(const char *str){
 		//"+000000000000"
 		_strValue[0] = '+';
 		_strValue[1] = '0';
+		count++;
 		return ;
 	}
 	//进入数字部分,
@@ -103,14 +111,14 @@ void BigData::_StringToData(const char *str){
 	if(symbol == '-'){
 		_intValue = 0-_intValue;
 	}
-	_strValue.resize(count+1+1);
 }
+
 //检查整型值是否溢出
 bool BigData::_CheckIsOverflow()const{
-	if(_strValue.size() > 21){
+	if(_strValue.size() > MaxSize){
 		//位数超过INT64最大值,一定溢出
 		return true;
-	}else if(_strValue.size() == 21){
+	}else if(_strValue.size() == MaxSize){
 		//位数相等
 		const char *Max = "+9223372036854775807";
 		const char *Min = "-9223372036854775808";
@@ -121,97 +129,59 @@ bool BigData::_CheckIsOverflow()const{
 	}
 	return false;
 }
-BigData BigData::operator+(const BigData& bd)const{
-	//判断两个数是否有溢出(可优化)
-	if(_CheckIsOverflow() || bd._CheckIsOverflow()){
-		//两者其中有一个溢出,用字符串进行相加运算
-		return BigData(_Add((std::string *)&_strValue,(std::string*)&bd._strValue).c_str());
-	}
 
-	//判断两个数之和是否溢出
-	//10 - 3 = 7   7 6 5 4 3 2 1			//正数
-	//-10 - -3 =-7  -7 -6 -5 -4 -3 -2 -1	//负数
-	if(_strValue[0]=='+' && bd._strValue[0]=='+'&& MaxValue-_intValue < bd._intValue || 
-		_strValue[0]=='-' && bd._strValue[0]=='-'&& MinValue - _intValue > bd._intValue){
-		//两个数之和溢出,用字符串进行相加运算
-			return BigData(_Add((std::string *)&_strValue,(std::string*)&bd._strValue).c_str());
-	}
-	//剩下情况,计算结果不会溢出,用整型计算
-	return BigData(_intValue+bd._intValue);
-}
-std::string BigData::_Add(std::string *left,std::string* right){
-	int LSize = left->size();
-	int RSize = right->size();
-	//判断左右操作数的位数
-	if(RSize > LSize){
-		//确保左操作数的位数大于右操作数
-		std::swap(left,right);
-		std::swap(LSize,RSize);
-	}
-	//对两种异号进行处理
-	if((*left)[0]=='+'&&(*right)[0]=='-'){
-		//即222222+ -111,对右操作数进行变号,调用减法
-		(*right)[0] = '+';
-		std::string ret  =  _Sub(left,right);
-		(*right)[0] = '-';
-		return ret;
-	}else if((*left)[0] == '-' && (*right)[0]=='+'){
-		//即-222222 + 1111,对左操作数进行变号,调用减法
-		(*left)[0] = '+';
-		std::string ret = _Sub(left,right);
-		(*left)[0] = '-';
-		ret[0] = '-';
-		return ret;
-	}
-	//剩下的就是同号情况,且左操作数的长度>=右操作数长度
-	int step = 0;
-	std::string sum;
-	sum.resize(LSize+1);
-	//进行计算
-	for(int i=0; i<LSize-2; ++i){
-		//减掉符号位和\0
-		int l = (*left)[LSize-i-1-1] - '0';	//取左操作数没有处理的最低位
-		int ret = 0;
-		//if(i<RSize){
-		if(RSize-i-1-1 > 0){
-			//右操作数,还没到符号位的
-			int r = (*right)[RSize-i-1-1] - '0';	//取右操作数没有处理的最低位
-			ret = l+r+step;				//进行相加					//进位后的值
-		}else{
-			//右操作值已经完了
-			ret = l+step;
-		}
-		step = ret/10;					//获取新进位
-		ret %= 10;	
-		sum[LSize-i-1] = ret + '0';		//将进位后的值放入和里面
-	}
-	sum[0] = (*left)[0];
-	sum[1] = step + '0';
-	return (char*)sum.c_str();
-}
 
 BigData BigData::operator-(const BigData & bd)const{
-	//两者存在溢出
-	if(_CheckIsOverflow() || bd._CheckIsOverflow()){
+	//两者其中之一溢出,或者都溢出
+	if(_CheckIsOverflow()||bd._CheckIsOverflow()){
 		//通过字符串进行加减
 		return BigData(_Sub((std::string *)&_strValue,(std::string*)&bd._strValue).c_str());
 	}
-	//异号
-	if(_strValue[0]!=bd._strValue[0]){
+
+	//两个都不溢出,其运算结果溢出(异号且位数等于19)
+	if(_strValue[0]!=bd._strValue[0] && 
+	_strValue.size()>=MaxSize|| bd._strValue.size()>=MaxSize){
 		//通过字符串进行加减
 		return BigData(_Sub((std::string *)&_strValue,(std::string*)&bd._strValue).c_str());
 	}
+
 	//不存在溢出且同号,结果一定不溢出,通过整型进行加减
 	return BigData(_intValue-bd._intValue);
+
 }
 std::string BigData::_Sub(std::string *left,std::string* right){
 	int LSize = left->size();
 	int RSize = right->size();
 	//判断左右操作数的位数
 	if(RSize > LSize){
-		//确保左操作数数值大于右操作数
-		std::swap(left,right);
-		std::swap(LSize,RSize);
+		//右操作数位数大于左操作数位数
+		std::string ret;
+		if((*left)[0]=='+'&&(*right)[0]=='+'){
+			//100 - 1000 = -(1000-100)
+			ret = _Sub(right,left);
+			ret[0] = '-';
+		}else if((*left)[0] =='-'&& (*right)[0] == '-'){
+			// -100 - -1000 = 1000-100
+			(*left)[0] = '+';
+			(*right)[0] = '+';
+			ret = _Sub(right,left);
+			(*left)[0] = '-';
+			(*right)[0] = '-';
+		}else if((*left)[0] == '+' && (*right)[0] == '-'){
+			//100 - -1000   1000 + 100
+			(*right)[0] = '+';
+			ret = _Add(right,left);
+			(*right)[0] = '-';
+		}else if((*left)[0] == '-' && (*right)[0] == '+'){
+			//-100 - 1000
+			(*left)[0] = '+';
+			ret = _Add(right,left);
+			(*left)[0] = '-';
+			ret[0] = '-';
+		}else{
+			assert(false);
+		}
+		return ret;
 	}
 	//对两种异号进行处理
 	if((*left)[0]=='+'&&(*right)[0]=='-'){
@@ -265,7 +235,7 @@ std::string BigData::_Sub(std::string *left,std::string* right){
 	}
 	sum[0] = (*left)[0];
 	sum[1] = step + '0';
-	return (char*)sum.c_str();
+	return sum.c_str();
 }
 
 

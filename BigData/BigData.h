9ee3eb2921c300630308ca67d4487dd5cbeac08d
gaoxiaodiao <805888196@qq.com>
2017-04-23 22:09:33 +0800

    大数据运算相关声明

--- BigData/BigData.h
+++ BigData/BigData.h
@@ -6,9 +6,11 @@
 */
 #include<string>
 #include<cstdlib>
+#include<cassert>
 typedef long long INT64;
 const INT64 MaxValue = 9223372036854775807;
 const INT64 MinValue = -9223372036854775808;
+const size_t MaxSize = 21;	//最大整型19位 + 符号位 + '\0'
 class BigData{
 friend	std::ostream &operator<<(std::ostream &,const BigData &);
 friend	void TestBigData3();
@@ -19,18 +21,21 @@ public:
 public:
 	BigData operator+(const BigData& )const;
 	BigData operator-(const BigData& )const;
+	BigData operator*(const BigData& )const;
+	BigData operator/(const BigData& )const;
 protected:
-	//BigData _Add(BigData&);
 	static std::string _Add(std::string *,std::string *);
 	static std::string _Sub(std::string *,std::string *);
+	static std::string _Mul(std::string *,std::string *);
+	static std::string _Div(std::string *,std::string *);
 	//检查是否溢出
 	bool _CheckIsOverflow()const;
 	//删除多余的空格与0
 	char* _DeleteBlank(const char* str);
 	//构造函数专用
-	void _StringToData(const char*str);
+	void _StringToData(const char*str,int&);
 	//将64位整型转为字符串
-	void _Int64ToString(INT64);
+	void _Int64ToString(INT64,int&);
 private:
 	INT64 _intValue;
 	std::string _strValue;
